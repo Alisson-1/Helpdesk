460944050a51fcc9de13c3c0fc0ce8f0855e99dd
Alisson-01 <alissongabrielassuncao@gmail.com>
2023-01-29 21:17:22 -0300

    atualizando sidebar

--- helpdeskfc/src/Components/SideBarLeft/index.tsx
+++ helpdeskfc/src/Components/SideBarLeft/index.tsx
@@ -1,9 +1,18 @@
-import { ContainerSideBar } from "./styles";
+import { ContainerSideBar, ContainerIcon,} from "./styles";
 import { FCLogo } from "./svg/fc";
+import UserIcon from './svg/UserIcon.svg'
 export const SideBarLeft = () => {
     return(
        <ContainerSideBar>
+      
         <FCLogo/>
+       
+        <ContainerIcon>
+
+        </ContainerIcon>
+       
+        <img src ={UserIcon}></img>
+        
        </ContainerSideBar> 
     );
 }
--- helpdeskfc/src/Components/SideBarLeft/styles.tsx
+++ helpdeskfc/src/Components/SideBarLeft/styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 export const ContainerSideBar = styled.div`
 height:100vh;
 width: 10rem;
@@ -8,8 +9,18 @@ box-shadow: 0px 10px 19px rgba(178, 179, 181, 0.7);
 display: flex;
 flex-direction: column;
 align-items:center;
+justify-content: space-between;
+padding: 1.5rem 0em 1.5em
+
+
+
 `
 
 export const ContainerIcon = styled.div`
+background-color: black;
+width:10rem;
+height: 70vh;
 
+;
 `
+
